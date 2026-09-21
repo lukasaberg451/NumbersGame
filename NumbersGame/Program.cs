f7613487d0c669640420962c79fe49e9e3dd182a
lukas <lukas@LUKAS>
2026-09-21 19:26:57 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempts = 0;
+            bool isGuessCorrect = false;
+            int randomNumber = new Random().Next(1, 21);
+
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan 1 och 20. Kan du gissa vilket? Du får fem försök.");
+
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userSelection))
+                {
+                    Console.WriteLine("Fel inmatning. Du måste enge ett heltal mellan 1 och 20.");
+                    continue;
+                }
+
+                isGuessCorrect = Utilities.CheckGuess(randomNumber, userSelection);
+
+                attempts++;
+            }
+            while (attempts != 5 && isGuessCorrect != true);
+            
+            if (attempts == 5 && isGuessCorrect == false)
+            {
+                Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök");
+            }
+        }
+    }
+}
---
+++ NumbersGame/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumbersGame
+{
+    internal class Utilities
+    {
+        public static bool CheckGuess(int randomNumber, int userSelection)
+        {
+            bool isGuessCorrect;
+            if (userSelection < randomNumber)
+            {
+                Console.WriteLine("Tyvärr du gissade för lågt");
+                isGuessCorrect = false;
+                return isGuessCorrect;
+            }
+            else if (userSelection > randomNumber)
+            {
+                Console.WriteLine("Tyvärr du gissade för högt");
+                isGuessCorrect = false;
+                return isGuessCorrect;
+            }
+            else
+            {
+                Console.WriteLine("Wohoo! Du gjorde det!");
+                isGuessCorrect = true;
+                return isGuessCorrect;
+            }
+        }
+    }
+}
