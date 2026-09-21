5f96985f1630aa4bd6b3d42d555481f6bf441459
lukas <lukas@LUKAS>
2026-09-21 19:53:18 +0200

    Added functionality to let the user play again at the end of the game

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -4,30 +4,44 @@
     {
         static void Main(string[] args)
         {
-            int attempts = 0;
-            bool isGuessCorrect = false;
-            int randomNumber = new Random().Next(1, 21);
-
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan 1 och 20. Kan du gissa vilket? Du får fem försök.");
+            int wantsToExit;
 
             do
             {
-                if (!int.TryParse(Console.ReadLine(), out int userSelection))
+                Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan 1 och 20. Kan du gissa vilket? Du får fem försök.");
+                int attempts = 0;
+                bool isGuessCorrect = false;
+                int randomNumber = new Random().Next(1, 21);
+
+                do
                 {
-                    Console.WriteLine("Fel inmatning. Du måste enge ett heltal mellan 1 och 20.");
-                    continue;
+                    if (!int.TryParse(Console.ReadLine(), out int userSelection))
+                    {
+                        Console.WriteLine("Fel inmatning. Du måste enge ett heltal mellan 1 och 20.");
+                        continue;
+                    }
+
+                    isGuessCorrect = Utilities.CheckGuess(randomNumber, userSelection);
+
+                    attempts++;
+                }
+                while (attempts != 5 && isGuessCorrect != true);
+
+                if (attempts == 5 && isGuessCorrect == false)
+                {
+                    Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök");
                 }
 
-                isGuessCorrect = Utilities.CheckGuess(randomNumber, userSelection);
+                Console.WriteLine("Vill du fortsätta spela? Skriv 1 för att fortsätta och 9 för att avsluta.");
 
-                attempts++;
-            }
-            while (attempts != 5 && isGuessCorrect != true);
-            
-            if (attempts == 5 && isGuessCorrect == false)
-            {
-                Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök");
-            }
+                if (!int.TryParse(Console.ReadLine(), out wantsToExit))
+                {
+                    Console.WriteLine("Fel inmatning. Skriv 1 för att fortsätta och 9 för att avsluta");
+                    continue;
+                }
+            } while (wantsToExit != 9);
+
+            Console.WriteLine("Tack för att du spelade!");
         }
     }
 }
